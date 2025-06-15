05e7d2118b76a4875696f9631e1dee1c6146e9c1
coffeedave <david@starcrunch.co.uk>
2025-06-15 11:35:23 +0100

    added additiona screen driver code

--- examples/read-temperature-screen-example.py
+++ examples/read-temperature-screen-example.py
@@ -34,6 +34,21 @@ def read_temp():
     return temp_c
 
 # Main body of the program
+
+PCF8574_address = 0x27  # I2C address of the PCF8574 chip.
+PCF8574A_address = 0x3F  # I2C address of the PCF8574A chip.
+# Create PCF8574 GPIO adapter.
+try:
+    mcp = PCF8574_GPIO(PCF8574_address)
+except:
+    try:
+        mcp = PCF8574_GPIO(PCF8574A_address)
+    except:
+        print ('I2C Address Error !')
+        exit(1)
+# Create LCD, passing in MCP GPIO adapter.
+lcd = Adafruit_CharLCD(pin_rs=0, pin_e=2, pins_db=[4,5,6,7], GPIO=mcp)
+
 mcp.output(3,1)     # turn on LCD backlight
 lcd.begin(20,4)     # set number of LCD lines and columns
 lcd.setCursor(0,0)  # set cursor position
