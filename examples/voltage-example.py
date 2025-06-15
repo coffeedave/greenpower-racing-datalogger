d7721339765905cd765278fe1b9a1883b7353190
coffeedave <david@starcrunch.co.uk>
2025-06-15 15:48:53 +0100

    added loop

--- examples/voltage-example.py
+++ examples/voltage-example.py
@@ -6,6 +6,8 @@ import adafruit_ina260
 i2c = busio.I2C(board.SCL, board.SDA)
 ina260 = adafruit_ina260.INA260(i2c)
 
-print("Current:", ina260.current)
-print("Voltage:", ina260.voltage)
-print("Power:", ina260.power)
+while True:
+    print(
+        f"Current: {ina260.current:.2f} mA Voltage: {ina260.voltage:.2f} V Power:{ina260.power:.2f} mW"  # noqa: E501
+    )
+    time.sleep(1)
